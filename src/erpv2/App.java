4c88ffc6468a953917079ebaadcaf534c67bcd91
BIG <BIG@BIG-PC>
2018-04-23 19:38:52 +0300

    home_23apr2018

---
+++ src/erpv2/App.java
@@ -0,0 +1,24 @@
+package erpv2;
+
+public class App {
+    /*
+    private CreditCalculator calculator;
+
+    public void init() {
+        calculator = new CreditCalculator();
+    }
+
+    public void run() {
+        calculator.setAmount(500000);
+        calculator.setYears(3);
+        calculator.calculate();
+        System.out.println("Месячный платеж: " + calculator.getMonthlyPayment());
+    }
+
+    public static void main(String[] args) {
+        App application = new App();
+        application.init();
+        application.run();
+    }
+    */
+}
